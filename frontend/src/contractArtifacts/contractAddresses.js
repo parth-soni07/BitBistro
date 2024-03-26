9e189d59eaf51a87ce83892a1940556d15905b59
Ayush Khandelwal <ayushkhandelwalak28.com>
2024-03-26 23:49:14 +0530

    Added Project Posting Functionalities

---
+++ frontend/src/contractArtifacts/contractAddresses.js
@@ -0,0 +1,1 @@
+export const masterAddress = "0x672f4387943DccBa0f2E2e508E9af749E2CFc738";
--- frontend/src/projectForm.js
+++ frontend/src/projectForm.js
@@ -1,9 +1,55 @@
 import React, { useState } from "react";
 import "./css/projectForm.css";
 import Owner from "./assets/Freelancer-cuate.png";
+import biddingData from "../src/contractArtifacts/Bidding.json";
+import masterData from "../src/contractArtifacts/Master.json";
+import { masterAddress } from "../src/contractArtifacts/contractAddresses.js";
+const ethers = require("ethers")
+
+
 
 const ProjectForm = () => {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [biddingContractAddress, setbiddingContractAddress] = useState("");
+  const [projectName, setProjectName] = useState('');
+  const [projectDescription, setProjectDescription] = useState('');
+  const [projectMetrics, setProjectMetrics] = useState('');
+
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner();
+
+  // Master Contract
+  const masterAbi = masterData.abi;
+  const masterContract = new ethers.Contract(masterAddress, masterAbi, signer);
+
+
+  async function deployContract(event) {
+
+    console.log('Project Name:', projectName);
+    console.log('Project Description:', projectDescription);
+    console.log('Project Metrics:', projectMetrics);
+
+    event.preventDefault();
+    console.log("Deploying Project Bidding Contract...");
+
+    try {
+
+      const biddingAbi = biddingData.abi;
+      const biddingByteCode = biddingData.bytecode;
+      const biddingContractFactory = new ethers.ContractFactory(biddingAbi, biddingByteCode, signer);
+      const biddingContract = await biddingContractFactory.deploy(projectName, projectDescription, projectMetrics);
+      console.log("Bidding Contract deployed");
+      setbiddingContractAddress(await biddingContract.address);
+      console.log("Current Deployed Bidding Contract Address : ", biddingContractAddress);
+      console.log("Bidding Contract adding to Master...");
+      const signerAddress = await signer.getAddress();
+      await masterContract.addProject(signerAddress, biddingContractAddress);
+      console.log("Bidding Contract Successfully added to Master with address : ", biddingContractAddress);
+    } catch (error) {
+      console.log("error >>>>>>>>>>>>>>>>>>>>>>>>>>>:", error);
+    }
+  }
 
   const handleButtonClick = () => {
     setIsFlipped(!isFlipped);
@@ -18,10 +64,24 @@ const ProjectForm = () => {
         </button>
       </div>
       <div className="project-card project-back">
-        <form className="project-form">
-          <input type="text" placeholder="Project Name" />
-          <textarea placeholder="Project Description"></textarea>
-          <button className="post-button">Post</button>
+        <form className="project-form" onSubmit={deployContract}>
+          <input
+            type="text"
+            placeholder="Project Name"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+          />
+          <textarea
+            placeholder="Project Description"
+            value={projectDescription}
+            onChange={(e) => setProjectDescription(e.target.value)}
+          />
+          <textarea
+            placeholder="Project Metrics"
+            value={projectMetrics}
+            onChange={(e) => setProjectMetrics(e.target.value)}
+          />
+          <button className="post-button" type="submit">Post</button>
         </form>
       </div>
     </div>
