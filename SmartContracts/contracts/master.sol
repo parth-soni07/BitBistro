fa9ee0b7662a9723e49521683b363297e5cb1ad3
Ayush Khandelwal <ayushkhandelwalak28.com>
2024-03-27 14:17:36 +0530

    Updated Master Contract

--- SmartContracts/contracts/master.sol
+++ SmartContracts/contracts/master.sol
@@ -2,17 +2,33 @@
 pragma solidity ^0.8.0;
 
 contract Master {
-    // Mapping from an owner address to an array of project addresses
     mapping(address => address[]) public ownerToProjects;
+    
+    address[] public allOwners;
+    address[] public allProjects;
 
-    // Function to add a project address for a given owner address
     function addProject(address _owner, address _project) public {
-        // Push the project address into the mapping for the given owner address
         ownerToProjects[_owner].push(_project);
+        allProjects.push(_project);
+        if (!isOwnerRegistered(_owner)) {
+            allOwners.push(_owner);
+        }
     }
 
-    // Function to get all project addresses associated with a given owner address
     function getProjectsByOwner(address _owner) public view returns (address[] memory) {
         return ownerToProjects[_owner];
+    }
+    
+    function getAllProjects() public view returns (address[] memory) {
+        return allProjects;
+    }
+
+    function isOwnerRegistered(address _owner) internal view returns (bool) {
+        for (uint i = 0; i < allOwners.length; i++) {
+            if (allOwners[i] == _owner) {
+                return true;
+            }
+        }
+        return false;
     }
 }
